d8e564eaf954ae138f0b551e713f5e43680b35ce
selotape <RonVisbord@Gmail.com>
2014-08-04 23:27:43 +0000

    added old implementations of VDC Api converted desktop models to vdc desktops added start/suspend buttons and implemented logic

--- web/base/urls.py
+++ web/base/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('base.views',
     url(r'^$', 'client_home'),
     url(r'^desktops/create$', 'create_desktop'),
     url(r'^desktops/delete/(?P<desktop_name>\w{0,50})$', 'delete_desktop'),
+    url(r'^desktops/toggle/(?P<current_state>\w{0,50})/(?P<desktop_id>i-[0-9,a-z]{0,10})$', 'toggle_state'),
 )
 
 urlpatterns += patterns('',
--- web/base/views.py
+++ web/base/views.py
@@ -3,20 +3,21 @@ from django.http import HttpResponseRedirect
 from base.models import Desktop
 from base.forms import NewDesktopForm
 from django.contrib.auth.decorators import login_required
+from VDCConnection import VDCConnection
+from MachineDetails import MachineDetails
+from utils import switch
+import Consts
 
-
-
+vdc = VDCConnection()
 
 @login_required
 def client_home(request):
 
-    # get all users desktops
-    my_desktops = Desktop.objects.filter(owner=request.user.username)
-
+    my_desktops = vdc.get_all_machines()# tags = {'owner':request.user.username, })
+    
     form = NewDesktopForm()
 
     context = {
-	'app_name' : 'vdc',
         'my_desktops': my_desktops,
         'form' : form,
     }
@@ -24,20 +25,34 @@ def client_home(request):
 
 @login_required
 def create_desktop(request):
-    if request.method == 'POST': # If the form has been submitted...
+    if request.method == 'POST': 
         form = NewDesktopForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
-	    desktop = form.save(commit=False)
-            desktop.owner = request.user.username
-    	    desktop.save()
+        if form.is_valid():
+             ami_field = form['ami']
+             vdc.create_machine(form['name'], form['ami'].value(), 
+                 key_name='idc-ex2-kp', tags={'owner':request.user.username, })
         else:
             print 'bad form: ', str(form)
-    return HttpResponseRedirect('/') # Redirect home after POST
+    return HttpResponseRedirect('/') # Redirect home
 
 @login_required
-def delete_desktop(request, desktop_name):
-    if request.method == 'GET':
-        desktop = Desktop.objects.get(name=desktop_name)
-	if desktop != None:
-	    desktop.delete()
-    return HttpResponseRedirect('/') # Redirect home after DELETE
+def delete_desktop(request, desktop_id):
+    vdc.terminate_machine(desktop_id)
+    return HttpResponseRedirect('/') # Redirect home
+
+
+@login_required
+def toggle_state(request, current_state, desktop_id):
+    for case in switch(current_state):
+        if case(Consts.RUNNING):
+            # vdc.stop_usage(desktop_id)
+            print 'desktop was running. now shut down'
+            break
+        if case(Consts.STANDBY):
+            # vdc.start_usage(desktop_id)
+            print 'desktop was shutdown. now running'
+            break
+        if case(): # default
+            print "current_state \'"+current_state+"\' is unknown"
+    return HttpResponseRedirect('/') # Redirect home
+    
